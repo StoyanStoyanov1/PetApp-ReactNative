358207d6d92884978582a65b5d2fec11df1769bc
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-10-27 16:41:29 +0100

    setup

---
+++ my-app/app/_layout.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { Slot, Stack } from 'expo-router';
+const RooyLayout = () => {
+  return (
+   <Stack>
+    
+
+   </Stack>
+  )
+}
+
+export default RooyLayout
