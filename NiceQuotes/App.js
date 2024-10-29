f4c65af17075bbb84e1792494f178aa054df0a31
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-10-29 07:59:44 +0100

    add a new button

--- NiceQuotes/App.js
+++ NiceQuotes/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Probleme kann man niemals mit derselben Denkweise lösen, durch die sie entstanden sind.</Text>
+      <Text>-- Albert Einstein</Text>
+      <Button title="Nächstes Zitat" />
       <StatusBar style="auto" />
     </View>
   );
