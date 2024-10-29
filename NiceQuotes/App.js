264c6b5843e59ee690f70a5a7b580eabf881197e
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-10-29 08:30:59 +0100

    useState

--- NiceQuotes/App.js
+++ NiceQuotes/App.js
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
+const data = [
+  {text: "Man sieht nur mit dem Herzen gut. Das Wesentliche ist für die Augen unsichtbar.", 
+    author: "Antoine de Saint-Exupéry" },
+{ text: "Phantasie ist wichtiger als Wissen, denn Wissen ist begrenzt.", 
+  author: "Albert Einstein" },
+{ text: "Wer kämpft, kann verlieren. Wer nicht kämpft, hat schon verloren.", 
+  author: "Bertolt Brecht" },
+];
+
 export default function App() {
+
+  const [index, setIndex] = useState(0);
+
+  const qoute = data[index];
+
   return (
     <View style={styles.container}>
-      <Text>Probleme kann man niemals mit derselben Denkweise lösen, durch die sie entstanden sind.</Text>
-      <Text>-- Albert Einstein</Text>
-      <Button title="Nächstes Zitat" />
+      <Text>{qoute.text}</Text>
+      <Text>-- {qoute.author}</Text>
+      <Button title="Nächstes Zitat" 
+        onPress={() => setIndex((index + 1 ) % data.length)}/>
       <StatusBar style="auto" />
     </View>
   );
