43f0d877cb2f38210cc62e31357b58b474b5ecf5
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-10-31 08:08:30 +0100

    Modal

--- NiceQuotes/components/NewQoute.js
+++ NiceQuotes/components/NewQoute.js
@@ -1,10 +1,14 @@
-import { Text, View, Pressable } from "react-native";
+import { Modal, Text, View, Pressable } from "react-native";
 
 export default function NewQoute({visible, onCancel}) {
-    if (!visible) return null;
 
     return (
-        <View style={{marginBottom: 50}}>
+        <Modal 
+        visible={visible} 
+        onRequestClose={onCancel}
+        animationType="slide"
+        >
+            <View style={{marginBottom: 50}}>
             <Text style={{ 
                 borderWidth: 3, 
                 padding: 30, 
@@ -15,6 +19,7 @@ export default function NewQoute({visible, onCancel}) {
             <Pressable onPress={onCancel}>
                 <Text style={{fontSize: 24, padding: 10}}>Abbrechen</Text>
             </Pressable>
-        </View>
+            </View>
+        </Modal>
     )
 }
