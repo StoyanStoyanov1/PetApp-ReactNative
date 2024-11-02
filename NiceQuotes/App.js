db07f6f6efb6b104f166aaf7c609cc0375a4b69a
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-11-02 21:41:11 +0100

    update

--- NiceQuotes/App.js
+++ NiceQuotes/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Alert, Text} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import Qoute from './components/Qoute';
@@ -8,22 +8,15 @@ import NewQoute from './components/NewQoute';
 import BigButton from './components/BigButton';
 import IconButton from './components/IconButtons';
 
-const data = [
-  { text: "Man sieht nur mit dem Herzen gut. Das Wesentliche ist für die Augen unsichtbar.", author: "Antoine de Saint-Exupéry" },
-  { text: "Phantasie ist wichtiger als Wissen, denn Wissen ist begrenzt.", author: "Albert Einstein" },
-  { text: "Wer kämpft, kann verlieren. Wer nicht kämpft, hat schon verloren.", author: "Bertolt Brecht" },
-];
-
 export default function App() {
   const [index, setIndex] = useState(0);
   const [showNewDialog, setShowNewDialog] = useState(false);
-  const [quotes, setQuotes] = useState(data);
+  const [quotes, setQuotes] = useState({});
 
   useEffect(() => {
     loadQuotes();
   }, []);
 
-  const quote = quotes[index];
 
   function addQuoteToList(name, content) {
     const newQuotes = [...quotes, { text: content, author: name }];  
@@ -42,7 +35,6 @@ export default function App() {
 
     if (quotesFromDB) {
       quotesFromDB = JSON.parse(quotesFromDB);
-      console.log('nach JSON.parse: ' + quotesFromDB.length);
       setQuotes(quotesFromDB);
     }
   }
@@ -50,29 +42,44 @@ export default function App() {
   function removeQuoteFromList() {
     const newQuotes = [...quotes];
     newQuotes.splice(index, 1);
-    setQuotes(newQuotes);
     setIndex(0);
+    setQuotes(newQuotes);
     saveQuotes(newQuotes);
   }
 
+  function deleteQoute () {
+    Alert.alert(
+      'Zitat löschen',
+      'Soll das Zitat wirklich gelöscht werden?',
+      [
+        {text: 'Abbrechen', style: 'cancel'}, 
+        {text: 'Bestätigen', style: 'destructive', onPress: removeQuoteFromList}
+      ]
+    )
+  }
+
+  const quote = quotes[index] ? quotes[index] : {};
+
   return (
     <View style={styles.container}>
-       <IconButton 
-        onPress={removeQuoteFromList}  
+       {quotes.length > 0 && <IconButton 
+        onPress={deleteQoute}  
         style={styles.delete} 
         icon="delete"  
-      />
-      <IconButton 
+      />}
+      {<IconButton 
         onPress={() => setShowNewDialog(!showNewDialog)}  
         style={styles.createButton} 
         icon="add-circle"  
-      />
+      />}
       <NewQoute 
         visible={showNewDialog} 
         onCancel={() => setShowNewDialog(false)} 
         onSave={addQuoteToList}
       />
-      <Qoute text={quote.text} author={quote.author}></Qoute>
+      {quotes.length && <Qoute text={quote.text} author={quote.author}> </Qoute> 
+      || <Text style={styles.noQuotes}>Keine Zitate</Text>
+      }
       <BigButton 
         title="Nächstes Zitat" 
         onPress={() => setIndex((index + 1) % quotes.length)} 
@@ -107,4 +114,8 @@ const styles = StyleSheet.create({
     top: 60,
     left: 30
   },
+  noQuotes: {
+    fontSize: 26,
+    color: 'darkred'
+  }
 });
