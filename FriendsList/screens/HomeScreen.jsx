ca82bc97b7fcd358d6ca2332c247b6fa0e8c7764
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-11-06 08:38:01 +0100

    Activity Indicator

--- FriendsList/screens/HomeScreen.jsx
+++ FriendsList/screens/HomeScreen.jsx
@@ -1,15 +1,39 @@
-import {FlatList, Button, StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { FlatList, StyleSheet, View, Text, ActivityIndicator } from 'react-native';
 import FriendListItem from '../components/FriendListItem';
 
+const dummyData = [
+  {first: 'Alice', last: 'Smith', email: 'test1@example.com'},
+  {first: 'Bob', last: 'Smith', email: 'test2@example.com'},
+  {first: 'Jane', last: 'Smith', email: 'test3@example.com'},
+  {first: 'Joe', last: 'Smith', email: 'test4@example.com'},
+];
+
 export default function HomeScreen({navigation}) {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchData() {
+          await new Promise((_) => setTimeout(_, 3000));
+          setData(dummyData);
+          setIsLoading(false);
+        }
+
+        fetchData();
+        
+    }, [])
+
+    if (isLoading) {
+      return <View 
+      style={styles.center}>
+        <ActivityIndicator size='large' color='darkorange' />
+      </View>
+    }
+
     return (
       <View style={styles.container}>
-        <FlatList data={[
-            {first: 'Alice', last: 'Smith', email: 'test1@example.com'},
-            {first: 'Bob', last: 'Smith', email: 'test2@example.com'},
-            {first: 'Jane', last: 'Smith', email: 'test3@example.com'},
-            {first: 'Joe', last: 'Smith', email: 'test4@example.com'},
-        ]}
+        <FlatList data={data}
         renderItem={({item}) => {
             return <FriendListItem 
             friend={item} onPress={() => {
@@ -17,6 +41,7 @@ export default function HomeScreen({navigation}) {
         }}
         keyExtractor={(item) => item.email}
         ItemSeparatorComponent={<View style={styles.listSeparator}/>}
+        ListEmptyComponent={<Text style={styles.listEmpty}>Keine Daten geladen</Text>}
         />
       </View>
     );
@@ -31,6 +56,16 @@ const styles = StyleSheet.create({
     listSeparator: {
         height: StyleSheet.hairlineWidth, 
         backgroundColor: 'lightsalmon',
+    },
+    listEmpty: {
+      fontSize: 32,
+      paddingTop: 100,
+      textAlign: 'center'
+    },
+    center: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
     }
 });
   
